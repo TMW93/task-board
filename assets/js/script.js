93ca937aba087e4975309c926adb9664da711abc
TimWong <timwong.tw93@gmail.com>
2024-03-31 16:08:07 +1100

    form data now saves to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+const taskFormEl = $(`#task-form`);
+const taskTitleInputEl = $(`#task-title`);
+const dateInputEl = $(`#task-due-date`);
+const taskDescriptInputEl = $(`#task-descript`);
+const todoCard = $(`#to-do-cards`);
+const inProgCard = $(`#in-progress-cards`);
+const doneCard = $(`#done-cards`);
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -10,11 +18,25 @@ function generateTaskId() {
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 
+  let currentTask = {
+    taskTitle: taskTitleInputEl.val(),
+    taskDueDate: dateInputEl.val(),
+    taskDescript: taskDescriptInputEl.val(),
+  };
+
+  if(!currentTask.taskTitle || !currentTask.taskDueDate || !currentTask.taskDescript) {
+    return;
+  } else {
+    taskList.push(currentTask);
+    localStorage.setItem(`tasks`, JSON.stringify(taskList));
+  }
 }
+
+taskFormEl.on(`submit`, createTaskCard);
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+  let currentList = taskList;
 }
 
 // Todo: create a function to handle adding a new task
@@ -34,5 +56,9 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+  // changes the date input from text to a calendar
+  dateInputEl.datepicker({
+    changeMonth: true,
+    changeYear: true
+  });
 });
