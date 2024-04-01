f553b6d26f9ba589d2ff1b815cdd0afba9d5406e
TimWong <timwong.tw93@gmail.com>
2024-04-02 10:32:38 +1100

    moved droppable code to drop function

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,21 +57,6 @@ function createTaskCard(task) {
     snap: `#todo-cards, #in-progress-cards, #done-cards`,
     stack: `.task-card`
   });
-  toDoEl.droppable({
-    drop: function(e, ui) {
-      $(ui.draggable).appendTo(toDoEl);
-    }
-  });
-  inProgEl.droppable({
-    drop: function(e, ui) {
-      $(ui.draggable).appendTo(inProgEl);
-    }
-  });
-  doneEl.droppable({
-    drop: function(e, ui) {
-      $(ui.draggable).appendTo(doneEl);
-    }
-  });
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -80,8 +65,6 @@ function renderTaskList() {
   for(task in currentList) {
     createTaskCard(task);
   }
-
-  $(`.card`).draggable();
 }
 
 // Todo: create a function to handle adding a new task
@@ -119,7 +102,21 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+  toDoEl.droppable({
+    drop: function(e, ui) {
+      $(ui.draggable).appendTo(toDoEl);
+    }
+  });
+  inProgEl.droppable({
+    drop: function(e, ui) {
+      $(ui.draggable).appendTo(inProgEl);
+    }
+  });
+  doneEl.droppable({
+    drop: function(e, ui) {
+      $(ui.draggable).appendTo(doneEl);
+    }
+  });
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -130,6 +127,9 @@ $(document).ready(function () {
     changeYear: true
   });
 
+  // make lanes droppable
+  handleDrop();
+  
   // event listener for the add task button
   taskFormEl.on(`submit`, handleAddTask);
 });
