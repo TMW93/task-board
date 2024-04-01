445cad7619bc493761220507898edb9bacbdfa98
TimWong <timwong.tw93@gmail.com>
2024-04-01 17:19:51 +1100

    some code were in wrong functions, cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@ const taskFormEl = $(`#task-form`);
 const taskTitleInputEl = $(`#task-title`);
 const dateInputEl = $(`#task-due-date`);
 const taskDescriptInputEl = $(`#task-descript`);
-const todoCard = $(`#to-do-cards`);
+const cardEl = $(`#to-do-cards`);
 const inProgCard = $(`#in-progress-cards`);
 const doneCard = $(`#done-cards`);
 
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
+if(taskList === null) {
+  taskList = [];
+}
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -17,22 +22,31 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+  // make a card to appear on webpage
+  // cardEl.addClass(`card`);
+  // let cardBodyEl = $(`<div>`);
+  // cardBodyEl.addClass(`card-body`);
+  // cardBodyEl.appendTo(cardEl);
 
-  let currentTask = {
-    taskTitle: taskTitleInputEl.val(),
-    taskDueDate: dateInputEl.val(),
-    taskDescript: taskDescriptInputEl.val(),
-  };
+  // let cardHeaderEl = $(`<h5>`);
+  // cardHeaderEl.addClass(`card-title`);
+  // cardHeaderEl.text(currentTask.taskTitle);
+  // cardHeaderEl.appendTo(cardBodyEl);
 
-  if(!currentTask.taskTitle || !currentTask.taskDueDate || !currentTask.taskDescript) {
-    return;
-  } else {
-    taskList.push(currentTask);
-    localStorage.setItem(`tasks`, JSON.stringify(taskList));
-  }
+  // let cardTextEl = $(`<p>`);
+  // cardTextEl.addClass(`card-text`);
+  // cardTextEl.text(currentTask.taskDescript);
+  // cardTextEl.appendTo(cardBodyEl);
+
+  // let cardDateEl = $(`<p>`);
+  // cardDateEl.addClass(`card-text`);
+  // cardDateEl.text(currentTask.taskDueDate);
+  // cardDateEl.appendTo(cardBodyEl);
+
+  // let cardDelete = $(`<btn>`);
+  // cardDelete.text(`Delete`);
+  // cardDelete.appendTo(cardBodyEl);
 }
-
-taskFormEl.on(`submit`, createTaskCard);
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -41,7 +55,24 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+  event.preventDefault();
+  let currentTask = {
+    taskTitle: taskTitleInputEl.val(),
+    taskDueDate: dateInputEl.val(),
+    taskDescript: taskDescriptInputEl.val(),
+  };
 
+  if(!currentTask.taskTitle || !currentTask.taskDueDate || !currentTask.taskDescript) {
+    $(`#formModal`).modal(`hide`);
+    return;
+  } else {
+    console.log()
+    // push recently input task to tasklist array and store in local storage
+    taskList.push(currentTask);
+    localStorage.setItem(`tasks`, JSON.stringify(taskList));
+
+    $(`#formModal`).modal(`hide`);
+  }
 }
 
 // Todo: create a function to handle deleting a task
@@ -61,4 +92,7 @@ $(document).ready(function () {
     changeMonth: true,
     changeYear: true
   });
+
+  // event listener for the add task button
+  taskFormEl.on(`submit`, handleAddTask);
 });
